7e0b361ac2a9679ec8d08fe096b7a1b6a9d88c93
CJ <cjfritz9@gmail.com>
2022-08-30 10:37:08 -0600

    test

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
+import { Header, Login, Messages, Posts } from './components'
+
+const App = () => {
+    return(
+        <div>
+             <Header />
+             <Search />
+             <Posts />
+        </div>
+    )
+}
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
