45458b67f9f3675b8efbb92f46e77781896603cc
CJ <cjfritz9@gmail.com>
2022-08-31 14:07:16 -0600

    change

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { createRoot } from 'react-dom/client';
 import { Header, Login, Messages, Posts } from './components'
+import { fetchAllPosts } from './api';
 
 const App = () => {
+    const [title, setTitle] = ([])
+    const [body, setBody] = ([])
+
     return(
         <div>
              <Header />
-             <Search />
-             <Posts />
+             {/* <Search /> */}
+             {/* <Posts title={title} body={body} /> */}
+             <Login />
         </div>
     )
 }
 
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(<App />);
