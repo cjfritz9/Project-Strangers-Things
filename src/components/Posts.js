d5366dcfc507869a3ebfb05e5dfba83ca0bc4383
CJ <cjfritz9@gmail.com>
2022-08-31 14:01:55 -0600

    changes

---
+++ src/components/Posts.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { fetchAllPosts } from "../api";
+
+// export const Posts = ({ title, body }) => {
+//     const [postsList, setPostsList] = useState([])
+    
+    
+// }
+
